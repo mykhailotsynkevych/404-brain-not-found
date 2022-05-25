99def87bdfef1d4d5332c45b186329cfaf915191
Diakov Sasha <diakovsasha777@gmail.com>
2022-05-25 23:25:08 +0300

    fix svg sprite

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,24 +1,17 @@
-$('.slider-for-js').slick({
+$('[data-slider-for]').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
+  // arrows: false,
   // fade: true,
-  asNavFor: '.slider-nav-js',
+  asNavFor: '[data-slider-nav]',
 });
 
-$('.slider-nav-js').slick({
+$('[data-slider-nav]').slick({
   mobileFirst: true,
   slidesToShow: 3,
   slidesToScroll: 1,
-  // infinity: true,
-  // speed: 400,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  asNavFor: '.slider-for-js',
+  asNavFor: '[data-slider-for]',
   dots: false,
-  centerMode: true,
-  focusOnSelect: true,
-  centerPadding: '0px',
   nextArrow: $('.reviews__arrow--next'),
   prevArrow: $('.reviews__arrow--prev'),
   responsive: [
